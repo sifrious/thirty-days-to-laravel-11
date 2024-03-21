c123f9ef0045b44ba1d8229c8a887fa8b96cb2ee
mray <sifrious@mrays-MacBook-Pro.local>
2024-03-21 16:39:09 -0400

    ep 7: Autoloading, Namespaces and Models -- replaced jobs array with Job class

--- routes/web.php
+++ routes/web.php
@@ -3,43 +3,33 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+class Job {
+    public static function all():array
+    {
+        return [
+            ['id'=>1,
+            'title' => 'Director',
+            'salary' => '50,000'],
+            ['id'=>2,
+            'title' => 'Programmer',
+            'salary' => '10,000'],
+            ['id'=>3,
+            'title' => 'Teacher',
+            'salary' => '40,000']];
+    }
+}
+
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => [
-        ['id'=>1,
-        'title' => 'Director',
-        'salary' => '50,000'],
-        ['id'=>2,
-        'title' => 'Programmer',
-        'salary' => '10,000'],
-        ['id'=>3,
-        'title' => 'Teacher',
-        'salary' => '40,000']
-    ]]);
+    return view('jobs', ['jobs'=>Job::all()]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
-    $jobs = [
-        ['id'=>1,
-        'title' => 'Director',
-        'salary' => '50,000'],
-        ['id'=>2,
-        'title' => 'Programmer',
-        'salary' => '10,000'],~
-        ['id'=>3,
-        'title' => 'Teacher',
-        'salary' => '40,000']];
-    
-    // so that didn't work... 
-    // Arr::first($jobs, function ($job) use {$id}) {
-    //     return $jobs['id'] == $id;
-    // }
-
-    $job = Arr::first($jobs, 
+    $job = Arr::first(Job::all(), 
        fn($job) => $job['id'] == $id, 
        fn() => abort(404));
 
